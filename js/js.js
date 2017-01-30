c835ce43b39e8f0ca2db38e1f1c97fbbeb0d0b3f
socsom <socsom77@gmail.com>
2017-01-30 15:35:06 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -10,7 +10,7 @@ xhttp.send();
 function myFunction(xml) {
     var xmlDoc = xml.responseXML;
     document.getElementById("title").innerHTML =
-    xmlDoc.getElementById("pregunta_001").childNodes[1].innerHTML;
+    xmlDoc.getElementById("pregunta_001").getElementByTagName("option")[3];
     document.getElementById("title2").innerHTML =
     xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
     document.getElementById("title3").innerHTML =
