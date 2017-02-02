cb2b0f213e71d53cf54ea83792428f2dbc111629
socsom <socsom77@gmail.com>
2017-02-02 21:20:27 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -5,7 +5,7 @@ xhttp.onreadystatechange = function() {
         gestionarXml(this);
     }
 };
-xhttp.open("GET", "questions.xml", true);
+xhttp.open("GET", "xml/questions.xml", true);
 xhttp.send();
 //****************************************************************************************************
 // Recuperamos los datos del fichero XML xml/questions.xml
